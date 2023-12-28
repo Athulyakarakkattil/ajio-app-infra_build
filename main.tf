cda72592a22ab6b1a9d5323a3635e53048d52ab1
athulya <athulya@gmail.com>
2023-12-28 16:36:02 +0000

    infra version 1

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # Key Pair creation
 #---------------------------------------------------------
 
+
 resource "aws_key_pair" "auth_key" {
   key_name   = "${var.project_name}-${var.project_env}"
   public_key = file("athulyakey.pub")
